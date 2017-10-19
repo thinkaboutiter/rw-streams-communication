d560194486b0f8f58a081717766a9b2462b7414b
Boyan Yankov <boyan.yankov@igsoft.com>
2017-10-19 15:31:48 +0300

    updated `ChatRoom.swift` - added network configurations

--- client/DogeChat/DogeChat/Source/Screens/ChatRoom/Model/ChatRoom.swift
+++ client/DogeChat/DogeChat/Source/Screens/ChatRoom/Model/ChatRoom.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SimpleLogger
 
 class ChatRoom: NSObject {
 
@@ -16,4 +17,88 @@ class ChatRoom: NSObject {
     var username: String = ""
     let maxReadLength: UInt = 4096
     
+    // MARK: - Initialization
+    override init() {
+        super.init()
+    }
+    
+    deinit {
+        Logger.debug.message("\(String(describing: ChatRoom.self)) deinitialized!")
+    }
+}
+
+// MARK: - Network setup
+extension ChatRoom {
+
+    func setupNetworkCommunication() throws {
+        // socket streams
+        // they won't be automatically memory menaged
+        var readStream: Unmanaged<CFReadStream>?
+        var writeStream: Unmanaged<CFWriteStream>?
+        
+        // binding socket streams together and connecting to server
+        CFStreamCreatePairWithSocketToHost(kCFAllocatorDefault,
+                                           AppConstants.ServerDetails.host,
+                                           AppConstants.ServerDetails.port,
+                                           &readStream,
+                                           &writeStream)
+    
+        try self.storeRetainedReferences(for: readStream, and: writeStream)
+        try self.addStreamsPairToRunLoop(self.inputStream, self.outputStream)
+        try self.openStreamsPair(self.inputStream, self.outputStream)
+    }
+    
+    fileprivate func storeRetainedReferences(for readStream: Unmanaged<CFReadStream>?, and writeStream: Unmanaged<CFWriteStream>?) throws {
+        guard let valid_readStream: Unmanaged<CFReadStream> = readStream else {
+            let errorMessage: String = "Invalid \(String(describing: CFReadStream.self))"
+            Logger.error.message(errorMessage)
+            throw ChatRoom.ChatRoomError.General(reason: errorMessage)
+        }
+        guard let valid_writeStream: Unmanaged<CFWriteStream> = writeStream else {
+            let errorMessage: String = "Invalid \(String(describing: CFWriteStream.self))"
+            Logger.error.message(errorMessage)
+            throw ChatRoom.ChatRoomError.General(reason: errorMessage)
+        }
+        
+        // storing retained references
+        self.inputStream = valid_readStream.takeRetainedValue()
+        self.outputStream = valid_writeStream.takeRetainedValue()
+    }
+    
+    fileprivate func addStreamsPairToRunLoop(_ inputStream: InputStream?, _ outputStream: OutputStream?) throws {
+        try self.addToRunLoop(inputStream)
+        try self.addToRunLoop(outputStream)
+    }
+    
+    private func addToRunLoop(_ stream: Stream?) throws {
+        let valid_stream: Stream = try self.valid(stream)
+        valid_stream.schedule(in: .current, forMode: .commonModes)
+    }
+    
+    fileprivate func openStreamsPair(_ inputStream: InputStream?, _ outputStream: OutputStream?) throws {
+        try self.open(inputStream)
+        try self.open(outputStream)
+    }
+    
+    private func open(_ stream: Stream?) throws {
+        let valid_stream: Stream = try self.valid(stream)
+        valid_stream.open()
+    }
+    
+    private func valid(_ stream: Stream?) throws -> Stream {
+        guard let valid_stream: Stream = stream else {
+            let errorMessage: String = "Invalid \(String(describing: Stream.self)) object!"
+            Logger.error.message(errorMessage)
+            throw ChatRoom.ChatRoomError.General(reason: errorMessage)
+        }
+        return valid_stream
+    }
+}
+
+// MARK: - Errors
+extension ChatRoom {
+    
+    enum ChatRoomError: Error {
+        case General(reason: String)
+    }
 }
